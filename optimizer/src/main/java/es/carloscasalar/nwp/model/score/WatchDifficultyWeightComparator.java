30be6c09ac0c3521ac58760eedab31552d3587cc
Carlos Castillo <castillo.st@gmail.com>
2017-12-23 22:03:49 +0100

    Add sleep time weight to difficult comparator
    Also changed constructionHeuristicType from FIRST_FIT to FIRST_FIT_DECREASING

--- optimizer/src/main/java/es/carloscasalar/nwp/model/score/WatchDifficultyWeightComparator.java
+++ optimizer/src/main/java/es/carloscasalar/nwp/model/score/WatchDifficultyWeightComparator.java
@@ -1,18 +1,43 @@
 package es.carloscasalar.nwp.model.score;
 
+import es.carloscasalar.nwp.model.Character;
 import es.carloscasalar.nwp.model.Watch;
+import org.apache.commons.lang3.builder.CompareToBuilder;
 
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class WatchDifficultyWeightComparator implements Comparator<Watch> {
 
     @Override
-    public int compare(Watch o1, Watch o2) {
-        return sizeOfWatchfulCharacters(o1).compareTo(sizeOfWatchfulCharacters(o2));
+    public int compare(Watch watchA, Watch watchB) {
+        return new CompareToBuilder()
+                .append(sizeOfWatchfulCharacters(watchA), sizeOfWatchfulCharacters(watchB))
+                .append(sleepingTimesWeight(watchA), sleepingTimesWeight(watchB))
+                .build();
+    }
+
+    private int sleepingTimesWeight(Watch watch) {
+        List<Integer> sleepTimes = watch.getWatchfulCharacters() == null ? Collections.emptyList() : watch.getWatchfulCharacters().stream()
+                .map(Character::getRequiredSleepTime)
+                .collect(Collectors.toList());
+
+        if(sleepTimes.isEmpty()){
+            return 0;
+        }
+
+        sleepTimes.sort(Integer::compareTo);
+
+        int minSleepTime = sleepTimes.get(0);
+        int maxSleepTime = sleepTimes.get(sleepTimes.size() - 1);
+
+        return minSleepTime == maxSleepTime ? 0 : (maxSleepTime-minSleepTime);
     }
 
     private Integer sizeOfWatchfulCharacters(Watch watch) {
-        if(watch.getWatchfulCharacters() == null){
+        if (watch.getWatchfulCharacters() == null) {
             return 0;
         }
 
--- optimizer/src/test/java/es/carloscasalar/nwp/model/score/WatchDifficultyWeightComparatorTest.java
+++ optimizer/src/test/java/es/carloscasalar/nwp/model/score/WatchDifficultyWeightComparatorTest.java
@@ -3,9 +3,14 @@ package es.carloscasalar.nwp.model.score;
 import es.carloscasalar.nwp.model.Watch;
 import es.carloscasalar.nwp.model.fixtures.CharacterFactory;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,6 +24,26 @@ public class WatchDifficultyWeightComparatorTest {
     public void init() {
         characterFactory = new CharacterFactory();
         comparator = new WatchDifficultyWeightComparator();
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void comparisons_between_empty_watches_should_not_fail() {
+        Watch watchA = new Watch();
+        Watch watchB = new Watch();
+
+        comparator.compare(watchA, watchB);
+
+        watchA.setWatchfulCharacters(Collections.emptyList());
+        comparator.compare(watchA, watchB);
+
+        watchB.setWatchfulCharacters(Collections.emptyList());
+        comparator.compare(watchA, watchB);
+
+        watchA.setWatchfulCharacters(Collections.singletonList(characterFactory.getHuman("Human A")));
+        comparator.compare(watchA, watchB);
     }
 
     @Test
@@ -62,12 +87,12 @@ public class WatchDifficultyWeightComparatorTest {
     @Test
     public void two_watch_with_the_same_number_of_character_weigh_the_same() {
         Watch twoWatchfulCharactersWatch = Watch.builder()
-                .watchfulCharacter(characterFactory.getDwarf("Character A"))
+                .watchfulCharacter(characterFactory.getElf("Character A"))
                 .watchfulCharacter(characterFactory.getElf("Character B"))
                 .build();
         Watch anotherWatchfulCharactersWatch = Watch.builder()
-                .watchfulCharacter(characterFactory.getHuman("Character C"))
-                .watchfulCharacter(characterFactory.getDwarf("Character D"))
+                .watchfulCharacter(characterFactory.getElf("Character C"))
+                .watchfulCharacter(characterFactory.getElf("Character D"))
                 .build();
 
         assertEquals(
@@ -76,6 +101,48 @@ public class WatchDifficultyWeightComparatorTest {
                 comparator.compare(
                         twoWatchfulCharactersWatch,
                         anotherWatchfulCharactersWatch
+                ));
+    }
+
+    @Test
+    public void if_number_of_characters_is_the_same_the_one_with_same_sleep_time_characters_should_be_lighter() {
+        Watch twoHumansWatch = Watch.builder()
+                .watchfulCharacter(characterFactory.getHuman("Human A"))
+                .watchfulCharacter(characterFactory.getHuman("Human B"))
+                .build();
+        Watch elfHumanWatch = Watch.builder()
+                .watchfulCharacter(characterFactory.getHuman("Human C"))
+                .watchfulCharacter(characterFactory.getElf("Elf A"))
+                .build();
+
+        assertEquals(
+                "Compare watch with two same sleeping time character against another with different sleeping time",
+                -1,
+                comparator.compare(
+                        twoHumansWatch,
+                        elfHumanWatch
+                ));
+    }
+
+    @Test
+    public void watches_with_greater_differences_between_sleep_times_should_weigh_more() {
+        Watch maxFourDifferenceSleepTime = Watch.builder()
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human D", 8))
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human E", 6))
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human F", 6))
+                .build();
+        Watch maxSixDifferenceSleepTime = Watch.builder()
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human A", 8))
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human B", 4))
+                .watchfulCharacter(characterFactory.getAlteredHuman("Human C", 2))
+                .build();
+
+        assertEquals(
+                "Compare watch with 2 hours of max difference against another with 6 hours of max difference",
+                -1,
+                comparator.compare(
+                        maxFourDifferenceSleepTime,
+                        maxSixDifferenceSleepTime
                 ));
     }
 }
