efe69772cfabc2b741a8394dd5a051f644123d7e
Carlos Castillo <castillo.st@gmail.com>
2018-02-03 22:54:48 +0100

    Remove unused attributes of PlanRequest

--- optimizer/src/main/java/es/carloscasalar/nwp/domain/PlanRequest.java
+++ optimizer/src/main/java/es/carloscasalar/nwp/domain/PlanRequest.java
@@ -18,23 +18,10 @@ import java.util.Set;
 @Data
 @Builder
 public class PlanRequest {
-    @JsonProperty("lengthOfWatch")
-    @NotNull
-    @Min(1)
-    private Integer lengthOfWatch;
-
     @JsonProperty("maxTotalTimeSpent")
     @NotNull
     @Min(60)
     private Integer maxTotalTimeSpent;
-
-    @JsonProperty("firstNightWatch")
-    @Min(1)
-    private Integer firstNightWatch;
-
-    @JsonProperty("lastNightWatch")
-    @Min(1)
-    private Integer lastNightWatch;
 
     @JsonProperty("party")
     @NotNull
