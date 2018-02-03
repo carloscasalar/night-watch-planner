16fd6157d68b0cfe5d5c196db641ed6a1f7b8bb3
Carlos Castillo <castillo.st@gmail.com>
2018-02-03 22:55:20 +0100

    Add sleeping charecter to WatchSummary

--- optimizer/src/main/java/es/carloscasalar/nwp/rest/PartyNightWatchSummary.java
+++ optimizer/src/main/java/es/carloscasalar/nwp/rest/PartyNightWatchSummary.java
@@ -1,26 +1,31 @@
 package es.carloscasalar.nwp.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import es.carloscasalar.nwp.domain.PartyNightWatch;
 import es.carloscasalar.nwp.domain.Plan;
+import lombok.Data;
 import org.optaplanner.core.api.score.buildin.hardmediumsoftlong.HardMediumSoftLongScore;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-
+@Data
 public class PartyNightWatchSummary {
-    @JsonProperty
+
     private final List<WatchSummary> watchesSummary;
-    @JsonProperty
+
+    private final Integer totalTime;
+
     private final HardMediumSoftLongScore score;
 
     public PartyNightWatchSummary(final Plan plan) {
-        this.watchesSummary = plan.compactedPartyNightWatch()
+        PartyNightWatch partyNightWatch = plan.compactedPartyNightWatch();
+        this.watchesSummary = partyNightWatch
                 .getWatches()
                 .stream()
-                .map(WatchSummary::new)
+                .map(watch -> new WatchSummary(watch, plan.getCharacters()))
                 .collect(Collectors.toList());
 
+        this.totalTime = partyNightWatch.totalTime();
         this.score = plan.getScore();
     }
 }
