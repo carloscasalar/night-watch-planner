bc524b17ff2e497f2d972c059705f3e893d49e22
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-02-06 00:14:47 +0100

    Init client dir
    Inspired by git@github.com:jaredpalmer/react-parcel-example.git

---
+++ client/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.less';
+
+const App = () => (
+  <div className="App">
+    <img className="App-Logo" src={logo} alt="React Logo" />
+    <h1 className="App-Title">Hello Parcel x React</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
