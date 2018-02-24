49292060ee74ee4eb23187d7b3fd30dabb15b7e8
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-02-24 22:38:18 +0100

    Add header and no character message

--- client/src/components/Party/Party.jsx
+++ client/src/components/Party/Party.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import './Party.less';
 
+import PartyHeader from './PartyHeader/PartyHeader';
 import Character from './Character/Character';
 import PartyControls from './PartyControls/PartyControls';
 
-const party = (props) => (
-    <div className="Party">
-        {props.characters.map(character =>
-            <Character
-                key={character.name}
-                character={character}
-                addSleepTime={props.addSleepTimeToCharacter}
-                updateName={(character, newName) => props.updateName(character, newName)}
-            />
-        )}
+const party = (props) => {
 
-        <PartyControls addCharacter={props.addCharacter}/>
-    </div>
-);
+    const noCharacterMessage = (
+        <div className="noCharacterMessage">
+            Push the add button (+) to add a character
+        </div>
+    );
+
+    return (
+        <div className="Party">
+            <PartyHeader />
+            {props.characters.length===0? noCharacterMessage : null }
+            {props.characters.map(character =>
+                <Character
+                    key={character.name}
+                    character={character}
+                    addSleepTime={props.addSleepTimeToCharacter}
+                    updateName={(character, newName) => props.updateName(character, newName)}
+                />
+            )}
+
+            <PartyControls addCharacter={props.addCharacter}/>
+        </div>
+    );
+}
 
 export default party;
---
+++ client/src/components/Party/PartyHeader/PartyHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './PartyHeader.less';
+
+const characterHeader = (props) => (
+        <div className="PartyHeader">
+            <div className="name">Name</div>
+            <div>Needs to Sleep</div>
+            <div>Actions</div>
+        </div>
+);
+
+export default characterHeader;
