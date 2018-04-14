994a35ae2a8d8e343f7d1a494b88084557a57843
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-04-14 22:03:41 +0200

    Fix linter errors in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.less';
 import Layout from './components/Layout/Layout';
 import PartyBuilder from './containers/PartyBuilder/PartyBuilder';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Layout >
-                    <PartyBuilder/>
-                </Layout>
-            </div>
-        );
+const app = () => (
+  <div className="App">
+    <Layout >
+      <PartyBuilder />
+    </Layout>
+  </div>
+);
 
-    }
-}
-
-export default App;
+export default app;
