69e0ccd7e4a91b4db7182ad50575b4a91ca365c9
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-04-14 22:10:07 +0200

    Fix linter errors in domain files

--- client/src/domain/PartyEntity.test.js
+++ client/src/domain/PartyEntity.test.js
@@ -2,38 +2,38 @@ import PartyEntity from './PartyEntity';
 import CharacterEntity from './CharacterEntity';
 import CharacterNotFoundException from './CharacterNotFoundException';
 
-const GANDALF = new CharacterEntity({id:1, name: 'Gandalf'});
-const GIMLI = new CharacterEntity({id:2, name: 'Gimli'});
+const GANDALF = new CharacterEntity({ id: 1, name: 'Gandalf' });
+const GIMLI = new CharacterEntity({ id: 2, name: 'Gimli' });
 
 test('should be able to instantiate a party with no members', () => {
-   const party = new PartyEntity();
+  const party = new PartyEntity();
 
-   expect(party.characters).toEqual([]);
+  expect(party.characters).toEqual([]);
 });
 
 test('should be able to instantiate a party with empty sample object', () => {
-   const party = new PartyEntity({});
+  const party = new PartyEntity({});
 
-   expect(party.characters).toEqual([]);
+  expect(party.characters).toEqual([]);
 });
 
 test('add character should return a party with a new character', () => {
-    const characters = [GANDALF];
-    const party = new PartyEntity({characters}).addCharacterWith(GIMLI.name);
-    const assertGimliExistsInParty = () => party.characters.find(({name}) => name === GIMLI.name);
-    expect(assertGimliExistsInParty).toBeTruthy();
+  const characters = [GANDALF];
+  const party = new PartyEntity({ characters }).addCharacterWith(GIMLI.name);
+  const assertGimliExistsInParty = () => party.characters.find(({ name }) => name === GIMLI.name);
+  expect(assertGimliExistsInParty).toBeTruthy();
 });
 
 test('remove a character sould return a party without the character', () => {
-   const characters = [GANDALF, GIMLI];
-   const party = new PartyEntity({characters}).removeCharacter(GIMLI);
+  const characters = [GANDALF, GIMLI];
+  const party = new PartyEntity({ characters }).removeCharacter(GIMLI);
 
-   expect(party.characters).toEqual([GANDALF]);
+  expect(party.characters).toEqual([GANDALF]);
 });
 
 test('update a non existing character should throw an exception', () => {
-    const characters = [GANDALF];
-    const tryTuUpdateNonExistingCharacter = () => new PartyEntity({characters}).updateCharacter(GIMLI);
+  const characters = [GANDALF];
+  const tryTuUpdateNonExistingCharacter = () => new PartyEntity({ characters }).updateCharacter(GIMLI);
 
-    expect(tryTuUpdateNonExistingCharacter).toThrow(CharacterNotFoundException);
+  expect(tryTuUpdateNonExistingCharacter).toThrow(CharacterNotFoundException);
 });
--- client/src/domain/stub/characters.js
+++ client/src/domain/stub/characters.js
@@ -1,4 +1,4 @@
-import CharacterEntity from "../CharacterEntity";
+import CharacterEntity from '../CharacterEntity';
 
-export const GANDALF = new CharacterEntity({id:1, name: 'Gandalf'});
-export const GIMLI = new CharacterEntity({id:2, name: 'Gimli'});
+export const GANDALF = new CharacterEntity({ id: 1, name: 'Gandalf' });
+export const GIMLI = new CharacterEntity({ id: 2, name: 'Gimli' });
