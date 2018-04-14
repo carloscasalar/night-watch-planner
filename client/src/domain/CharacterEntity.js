b0cf1fd34bde5cbb8d3a1175bdf88e45ed3b3405
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-04-14 20:26:44 +0200

    Fix linter in party and character entities

--- client/src/domain/CharacterEntity.js
+++ client/src/domain/CharacterEntity.js
@@ -6,30 +6,34 @@ const MIN_TIME = 0;
 export const DEFAULT_REQUIRED_SLEEP_MINUTES = 6 * 60;
 
 export default class CharacterEntity {
-    constructor({id = NO_ID, name = DEFAULT_NAME, requiredSleepTime = DEFAULT_REQUIRED_SLEEP_MINUTES}) {
-        if (id === NO_ID) {
-            throw new IdRequiredException();
-        }
-        this.id = id;
-        this.name = name;
-        this.requiredSleepTime = requiredSleepTime;
+  constructor({
+    id = NO_ID,
+    name = DEFAULT_NAME,
+    requiredSleepTime = DEFAULT_REQUIRED_SLEEP_MINUTES,
+  }) {
+    if (id === NO_ID) {
+      throw new IdRequiredException();
     }
+    this.id = id;
+    this.name = name;
+    this.requiredSleepTime = requiredSleepTime;
+  }
 
     toJSON = () => ({
-        id: this.id,
-        name: this.name,
-        requiredSleepTime: this.requiredSleepTime
+      id: this.id,
+      name: this.name,
+      requiredSleepTime: this.requiredSleepTime,
     });
 
     copy = () => new CharacterEntity(this.toJSON());
 
     updateName(name) {
-        this.name = name;
-        return this;
+      this.name = name;
+      return this;
     }
 
     increaseRequiredSleepTime(time) {
-        this.requiredSleepTime = Math.max(MIN_TIME, this.requiredSleepTime + time);
-        return this;
-    };
+      this.requiredSleepTime = Math.max(MIN_TIME, this.requiredSleepTime + time);
+      return this;
+    }
 }
--- client/src/domain/PartyEntity.js
+++ client/src/domain/PartyEntity.js
@@ -3,54 +3,54 @@ import CharacterNotFoundException from './CharacterNotFoundException';
 
 const NO_CHARACTERS = [];
 const EMPTY_PARTY = {
-    characters: NO_CHARACTERS
+  characters: NO_CHARACTERS,
 };
 
 export default class PartyEntity {
-    constructor({characters = NO_CHARACTERS} = EMPTY_PARTY) {
-        this.characters = characters;
+  constructor({ characters = NO_CHARACTERS } = EMPTY_PARTY) {
+    this.characters = characters;
+  }
+
+  get isEmpty() {
+    return this.characters.length === 0;
+  }
+
+  findCharacterById(characterId) {
+    return this.characters.find(({ id }) => id === characterId);
+  }
+
+  findCharacterByName(characterName) {
+    return this.characters.find(({ name }) => name === characterName);
+  }
+
+  copy() {
+    const characters = this.characters.map(character => character.copy());
+    return new PartyEntity({ characters });
+  }
+
+  addCharacterWith(name) {
+    const id = this.nextCharacterId();
+    const character = new CharacterEntity({ id, name });
+    this.characters.push(character);
+    return this;
+  }
+
+  updateCharacter(character) {
+    const characterIndex = this.characters.findIndex(({ id }) => id === character.id);
+    if (characterIndex < 0) {
+      throw new CharacterNotFoundException();
     }
 
-    get isEmpty(){
-        return this.characters.length === 0;
-    }
+    this.characters[characterIndex] = character;
+    return this;
+  }
 
-    findCharacterById(characterId){
-        return this.characters.find(({id}) => id === characterId);
-    }
+  removeCharacter(character) {
+    this.characters = this.characters.filter(({ id }) => id !== character.id);
+    return this;
+  }
 
-    findCharacterByName(characterName){
-        return this.characters.find(({name}) => name === characterName);
-    }
-
-    copy() {
-        const characters = this.characters.map(character => character.copy());
-        return new PartyEntity({characters});
-    }
-
-    addCharacterWith(name) {
-        const id = this.nextCharacterId();
-        const character = new CharacterEntity({id, name});
-        this.characters.push(character);
-        return this;
-    }
-
-    updateCharacter(character) {
-        const characterIndex = this.characters.findIndex(({id}) => id === character.id);
-        if (characterIndex < 0) {
-            throw new CharacterNotFoundException();
-        }
-
-        this.characters[characterIndex] = character;
-        return this;
-    };
-
-    removeCharacter(character) {
-        this.characters = this.characters.filter(({id}) => id !== character.id);
-        return this;
-    }
-
-    nextCharacterId() {
-        return this.characters.reduce((maxId, {id}) => Math.max(maxId, id), 0) + 1;
-    }
+  nextCharacterId() {
+    return this.characters.reduce((maxId, { id }) => Math.max(maxId, id), 0) + 1;
+  }
 }
