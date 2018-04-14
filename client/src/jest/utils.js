7a3f3a15f3e9d7600e14c04ef73539881dbf3d3c
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-04-14 22:09:07 +0200

    Fix linter error in jest utility files

--- client/src/jest/utils.js
+++ client/src/jest/utils.js
@@ -1,17 +1,17 @@
 const FIRST_MOCK_CALL = 0;
 const FIRST_ARGUMENT_CALL = 0;
-export const firstParameterOfFirstCallTo = ({mock}) => {
-    if(!mock){
-        throw new Exception('Is not a mock');
-    }
+export const firstParameterOfFirstCallTo = ({ mock }) => {
+  if (!mock) {
+    throw new Error('Is not a mock');
+  }
 
-    return mock.calls[FIRST_MOCK_CALL][FIRST_ARGUMENT_CALL];
+  return mock.calls[FIRST_MOCK_CALL][FIRST_ARGUMENT_CALL];
 };
 
-export const numberOfCallsTo = ({mock}) => {
-    if(!mock){
-        throw new Exception('is not a mock');
-    }
+export const numberOfCallsTo = ({ mock }) => {
+  if (!mock) {
+    throw new Error('is not a mock');
+  }
 
-    return mock.calls.length;
+  return mock.calls.length;
 };
