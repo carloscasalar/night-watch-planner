6bdbcac2fd39218aa7ca8bc4f2277f74f7a95888
Carlos Castillo Alarcón <castillo.st@gmail.com>
2018-04-21 00:08:32 +0200

    Refactor increaseMaxTotalTimeSpent reducer to use use case

--- client/src/domain/NightWatchConfigEntity.js
+++ client/src/domain/NightWatchConfigEntity.js
@@ -1,5 +1,6 @@
 const emptyConfig = {};
 const TWELVE_HOURS_IN_MINUTES = 12 * 60;
+const MIN_TOTAL_TIME_SPENT = 0;
 export const DEFAULT_MAX_TIME_SPENT = TWELVE_HOURS_IN_MINUTES;
 
 export default class NightWatchConfigEntity {
@@ -8,7 +9,7 @@ export default class NightWatchConfigEntity {
   }
 
   increaseMaxTotalTimeSpent(time) {
-    this.maxTotalTimeSpent = this.maxTotalTimeSpent + time;
+    this.maxTotalTimeSpent = Math.max(this.maxTotalTimeSpent + time, MIN_TOTAL_TIME_SPENT);
 
     return this;
   }
--- client/src/store/reducers/increaseMaxTotalTimeSpent.js
+++ client/src/store/reducers/increaseMaxTotalTimeSpent.js
@@ -1,9 +1,13 @@
-const MIN_TIME = 0;
+import NightWatchConfigRepository from '../../ports/config/NightWatchConfigRepository';
+import IncreaseMaxTotalTimeSpent from '../../usecases/config/IncreaseMaxTotalTimeSpent';
 
-const increaseMaxTotalTimeSpent = (state, { time }) => ({
-  ...state,
-  maxTotalTimeSpent: Math.max(MIN_TIME, state.maxTotalTimeSpent + time),
-});
+const increaseMaxTotalTimeSpent = (state, { time }) => {
+  const nightWatchConfigRepository = new NightWatchConfigRepository(state);
+  const increaseMaxTimeSpentUseCase = new IncreaseMaxTotalTimeSpent(nightWatchConfigRepository);
+  increaseMaxTimeSpentUseCase.execute(time);
+
+  return nightWatchConfigRepository.state;
+};
 
 export default increaseMaxTotalTimeSpent;
 
