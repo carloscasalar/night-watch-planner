72a7af7ed440bc5df68d11a53c8554d9b8ea88bb
Carlos Castillo <castillo.st@gmail.com>
2018-06-25 10:23:01 +0200

    Enable react dev tools
    Basic store https://github.com/zalmoxisus/redux-devtools-extension#11-basic-store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,7 +7,11 @@ import './index.less';
 import App from './App';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  // eslint-disable-next-line
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 
