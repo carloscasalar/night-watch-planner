ef39fcb4b9d845340b0b20cd396b6b4f5afdfd5b
Carlos Castillo <castillo.st@gmail.com>
2018-06-25 10:53:38 +0200

    Add thunk middleware
    Also update redux devtools configuration to https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.less';
 import App from './App';
 import reducer from './store/reducer';
 
-const store = createStore(
-  reducer,
-  // eslint-disable-next-line
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// eslint-disable-next-line
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 
