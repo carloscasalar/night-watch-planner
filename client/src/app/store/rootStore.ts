da91a5d9ac6b1c15f0461a671f0fea116b0000ac
Carlos Castillo <castillo.st@gmail.com>
2021-03-15 23:30:05 +0100

    Add ui reducer and root store

---
+++ client/src/app/store/rootStore.ts
@@ -0,0 +1,11 @@
+import { StateType } from 'typesafe-actions';
+import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { ui } from '../ui/reducer';
+
+const rootReducer = combineReducers({
+    ui,
+});
+export type RootState = StateType<typeof rootReducer>;
+
+export const rootStore = createStore(rootReducer, composeWithDevTools());
---
+++ client/src/app/ui/reducer.ts
@@ -0,0 +1,9 @@
+import { createReducer } from 'typesafe-actions';
+
+export interface UiState {
+    waitingForPlan: boolean;
+}
+
+export const ui = createReducer<UiState>({
+    waitingForPlan: false,
+});
