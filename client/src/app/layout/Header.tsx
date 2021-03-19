d952568eb821db29076bae19235b0227cb3da96d
Carlos Castillo <castillo.st@gmail.com>
2021-03-19 22:50:08 +0100

    Use text color to set the icon background color

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -5,7 +5,7 @@ export const Header: FC = () => (
   <header className="text-gray-600 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <Icon name="campfire" className="w-10 h-10 text-white rounded-full" />
+        <Icon name="campfire" className="w-10 h-10 text-black rounded-full" />
         <span className="ml-3 text-xl">Night Watch Planner</span>
       </div>
     </div>
