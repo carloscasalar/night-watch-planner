6dfaff28bf4a6c4cbdea0df6217fdeb25821b14a
Carlos Castillo <castillo.st@gmail.com>
2021-03-20 00:59:03 +0100

    Format hours and minutes

--- client/src/features/character/getFormattedCharacter.ts
+++ client/src/features/character/getFormattedCharacter.ts
@@ -2,12 +2,21 @@ import { createSelector } from 'reselect';
 import { Character, CharacterId, FormattedCharacter } from './schema';
 import { getCharacters } from '../party/getCharacters';
 
+const toHourMinutes = (minutes: number): string => {
+  const minutesRemaining = minutes % 60;
+  const hours = (minutes - minutesRemaining) / 60;
+
+  return minutesRemaining
+    ? `${hours} hours, ${minutesRemaining} minutes`
+    : `${hours} hours`;
+};
+
 const getId = (_status: unknown, id: CharacterId | undefined): CharacterId =>
   id || '';
 
 const toFormattedCharacter = (character: Character): FormattedCharacter => ({
   name: character.name,
-  requiredSleepTime: `${character.requiredSleepTime} minutes`,
+  requiredSleepTime: toHourMinutes(character.requiredSleepTime),
 });
 
 export const getFormattedCharacter = createSelector(
