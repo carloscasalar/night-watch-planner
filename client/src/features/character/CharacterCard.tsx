21b8df3eb67c6275665d26ddc3daf7c7fe15f4bb
Carlos Castillo <castillo.st@gmail.com>
2021-03-19 23:00:20 +0100

    Add a sandclock next to time

--- client/src/features/character/CharacterCard.tsx
+++ client/src/features/character/CharacterCard.tsx
@@ -18,13 +18,16 @@ export const CharacterCard: FC<CharacterCardProps> = ({ characterId }) => {
       <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg shadow-lg">
         <Icon
           name="barbarian"
-          className="w-12 h-12 rounded-full mr-4 bg-grey-300 text-black"
+          className="w-12 h-12 rounded-full mr-4 text-black"
         />
         <div className="flex-grow">
           <h2 className="text-gray-900 title-font font-medium">
             {character?.name}
           </h2>
-          <p className="text-gray-500">{character?.requiredSleepTime}</p>
+          <p className="text-gray-500 flex items-center">
+            <Icon name="sand-clock" className="w-4 h-4 inline-block" />
+            {character?.requiredSleepTime}
+          </p>
         </div>
       </div>
     )
