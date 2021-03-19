7a8b0acce411f8bdfcbee80db394fbe89922b5bd
Carlos Castillo <castillo.st@gmail.com>
2021-03-19 21:08:16 +0100

    Add campfire icon

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,29 +1,14 @@
 import { FC } from 'react';
+import { Icon } from '../../common/components/icon/Icon';
 
 export const Header: FC = () => (
   <header className="text-gray-600 body-font">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <Icon
+          name="campfire"
           className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
-          />
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
-          />
-        </svg>
+        />
         <span className="ml-3 text-xl">Night Watch Planner</span>
       </div>
     </div>
