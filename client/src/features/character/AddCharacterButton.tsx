3781ba568c070cdceb8c2fbd1e5b0df0631f0102
Carlos Castillo <castillo.st@gmail.com>
2021-03-20 19:12:16 +0100

    Add add character icon

--- client/src/features/character/AddCharacterButton.tsx
+++ client/src/features/character/AddCharacterButton.tsx
@@ -3,26 +3,12 @@ import { Icon } from '../../common/components/icon/Icon';
 
 export const AddCharacterButton: FC = () => {
   return (
-    <div className="h-full border-gray-200 border p-4 rounded-lg shadow-lg grid grid-cols-4 gap-1">
-      <div className="flex items-center cursor-pointer">
+    <div className="h-full flex items-center">
+      <div className="rounded-full border-gray-200 border rounded-lg shadow-lg hover:shadow-xl cursor-pointer">
         <Icon
-          name="barbarian"
-          className="w-6 h-6 rounded-full mr-4 text-black"
+          name="add-character"
+          className="w-16 h-16 p-4 self-center text-black"
         />
-        +
-      </div>
-      <div className="flex items-center cursor-pointer">
-        <Icon name="elf-ear" className="w-6 h-6 rounded-full mr-4 text-black" />
-        <span className="transform hover:translate-x-3">+</span>
-      </div>
-      <div className="flex items-center cursor-pointer">
-        <Icon name="rogue" className="w-6 h-6 rounded-full mr-4 text-black" /> +
-      </div>
-      <div className="flex items-center cursor-pointer">
-        <Icon name="rogue" className="w-6 h-6 rounded-full mr-4 text-black" /> +
-      </div>
-      <div className="flex items-center cursor-pointer">
-        <Icon name="rogue" className="w-6 h-6 rounded-full mr-4 text-black" /> +
       </div>
     </div>
   );
