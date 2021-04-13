276db2611ac93bc6153b0f43ac0a85313e2cc49a
Carlos Castillo <castillo.st@gmail.com>
2021-04-13 21:30:02 +0200

    Make add character button call the create the add character action

--- client/src/features/character/AddCharacterButton.tsx
+++ client/src/features/character/AddCharacterButton.tsx
@@ -1,15 +1,29 @@
 import { FC } from 'react';
 import { Icon } from '../../common/components/icon/Icon';
+import { useDispatch } from 'react-redux';
+import { addCharacterAction } from './addCharacterAction';
 
 export const AddCharacterButton: FC = () => {
+  const dispatch = useDispatch();
+
+  const addCharacter = () =>
+    dispatch(
+      addCharacterAction({
+        name: 'Rename me!',
+      }),
+    );
+
   return (
     <div className="h-full flex items-center opacity-25 hover:opacity-100">
-      <div className="ml-2 rounded-full border-gray-200 border shadow-lg hover:shadow-xl cursor-pointer">
+      <button
+        className="ml-2 rounded-full border-gray-200 border shadow-lg hover:shadow-xl cursor-pointer focus:outline-none"
+        onClick={addCharacter}
+      >
         <Icon
           name="add-character"
           className="w-24 h-24 p-4 self-center text-black"
         />
-      </div>
+      </button>
     </div>
   );
 };
