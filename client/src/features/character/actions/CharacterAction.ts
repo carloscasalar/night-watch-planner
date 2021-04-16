1766ce08dd0da344135dcbb2c3b4ebd358cbe8e8
Carlos Castillo <castillo.st@gmail.com>
2021-04-16 21:44:13 +0200

    Implement remove character action and use case

--- client/src/features/character/actions/CharacterAction.ts
+++ client/src/features/character/actions/CharacterAction.ts
@@ -2,10 +2,12 @@ import { increaseCharacterSleepTimeAction } from './increaseCharacterSleepTimeAc
 import { ActionType } from 'typesafe-actions';
 import { updateCharacterNameAction } from './updateCharacterNameAction';
 import { addCharacterAction } from './addCharacterAction';
+import { removeCharacterAction } from './removeCharacterAction';
 
 const ActionCreators = {
   addCharacterAction,
   increaseCharacterSleepTimeAction,
+  removeCharacterAction,
   updateCharacterNameAction,
 };
 
