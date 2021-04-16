79424b6ccdf5a553989fd374323797e15f01b29e
Carlos Castillo <castillo.st@gmail.com>
2021-04-16 22:10:02 +0200

    🔥 Remove sample characters from the party reducer

--- client/src/features/party/reducer.ts
+++ client/src/features/party/reducer.ts
@@ -16,29 +16,8 @@ export interface PartyState {
 }
 
 export const party = createReducer<PartyState, RootAction>({
-  characters: {
-    gandalf: {
-      id: 'gandalf',
-      name: 'Gandalf',
-      requiredSleepTime: 5 * 60,
-    },
-    legolas: {
-      id: 'legolas',
-      name: 'Legolas',
-      requiredSleepTime: 4 * 60 + 30,
-    },
-    boromir: {
-      id: 'boromir',
-      name: 'Boromir',
-      requiredSleepTime: 6 * 60,
-    },
-    frodo: {
-      id: 'frodo',
-      name: 'Frodo',
-      requiredSleepTime: 6 * 60,
-    },
-  },
-  order: ['gandalf', 'legolas', 'frodo', 'boromir'],
+  characters: {},
+  order: [],
 })
   .handleAction(addCharacterAction, (state, { payload: { name } }) => {
     const repository = new PartyStateRepository(state);
