54ce659434f7f7407f99194414f3432e8bd27b3c
Carlos Castillo <castillo.st@gmail.com>
2021-04-21 18:42:25 +0200

    Configure the epic middleware

--- client/src/app/store/rootStore.ts
+++ client/src/app/store/rootStore.ts
@@ -1,13 +1,27 @@
 import { StateType } from 'typesafe-actions';
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { party } from '../../features/party/reducer';
 import { config } from '../../features/config/reducer';
+import { createEpicMiddleware } from 'redux-observable';
+import { ApplicationAction } from './ApplicationAction';
+import { rootEpic } from './epics/rootEpic';
 
 const rootReducer = combineReducers({
   party,
   config,
 });
+
 export type RootState = StateType<typeof rootReducer>;
 
-export const rootStore = createStore(rootReducer, composeWithDevTools());
+const epicMiddleware = createEpicMiddleware<
+  ApplicationAction,
+  ApplicationAction,
+  RootState
+>();
+
+const composeEnhancers = composeWithDevTools({ trace: true });
+const enhancers = composeEnhancers(applyMiddleware(epicMiddleware));
+export const rootStore = createStore(rootReducer, {}, enhancers);
+
+epicMiddleware.run(rootEpic);
