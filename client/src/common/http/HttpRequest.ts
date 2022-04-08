b7f544247a12e26027026b923f51ef9763596332
Carlos Castillo <castillo.st@gmail.com>
2022-04-08 18:25:17 +0200

    Add type for ajax methods

--- client/src/common/http/HttpRequest.ts
+++ client/src/common/http/HttpRequest.ts
@@ -1,6 +1,15 @@
+import { ajax } from 'rxjs/ajax';
+
+export type HttpMethod = 'GET' | 'POST';
+
 export interface HttpRequest<RequestPayload = unknown> {
-  method: 'GET' | 'POST';
+  method: HttpMethod;
   url: string;
   payload?: RequestPayload;
   headers?: Record<string, string>;
 }
+
+export const ajaxMethods: Record<HttpMethod, keyof typeof ajax> = {
+  GET: 'get',
+  POST: 'post',
+};
