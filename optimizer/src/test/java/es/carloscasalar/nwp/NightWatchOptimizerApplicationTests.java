4bd7ac33252071d46e659eb0869b334c10958d69
carlos.castillo <carlos.castillo@cabify.com>
2022-04-08 21:37:30 +0200

    Upgrade spring boot versions

--- optimizer/src/test/java/es/carloscasalar/nwp/NightWatchOptimizerApplicationTests.java
+++ optimizer/src/test/java/es/carloscasalar/nwp/NightWatchOptimizerApplicationTests.java
@@ -1,16 +1,13 @@
 package es.carloscasalar.nwp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class NightWatchOptimizerApplicationTests {
+class NightWatchOptimizerApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
