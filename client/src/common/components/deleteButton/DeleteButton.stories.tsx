67cd39e96109d565b5b55ce5dc025938ea006042
Carlos Castillo <castillo.st@gmail.com>
2022-04-13 21:21:56 +0200

    Add storibook file for delete button

---
+++ client/src/common/components/deleteButton/DeleteButton.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, Story } from '@storybook/react';
+import { DeleteButton, DeleteButtonProps } from './DeleteButton';
+
+const stories: Meta = {
+  title: 'Common/DeleteButton',
+  component: DeleteButton,
+};
+
+export default stories;
+
+export const DeleteButtonStory: Story<DeleteButtonProps> = ({
+  className,
+  onClick,
+}) => <DeleteButton className={className} onClick={onClick} />;
+
+DeleteButtonStory.args = {
+  className: 'cursor-pointer',
+};
+
+DeleteButtonStory.argTypes = {
+  onClick: { action: 'button clicked' },
+};
