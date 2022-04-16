6ded924ff02a0e53dc150359d2dd4ffcee44ab08
Carlos Castillo <castillo.st@gmail.com>
2022-04-17 00:36:43 +0200

    Initialize a story for a plan with a result

--- client/src/features/plan/PlanPanel.stories.tsx
+++ client/src/features/plan/PlanPanel.stories.tsx
@@ -2,8 +2,8 @@ import {
   Story,
   Meta,
 } from '@storybook/react/dist/ts3.9/client/preview/types-6-0';
-import { PlanPanel } from './PlanPanel';
 import { RootState } from 'typesafe-actions';
+import { PlanPanel } from './PlanPanel';
 import { testRootState } from '../../test/utils/testRootState';
 import { TestAppWrapper } from '../../test/utils/TestAppWrapper';
 
@@ -71,4 +71,89 @@ export const PlanPanelErrorStory: Story<PlanPanelErrorStoryProps> = ({
 
 PlanPanelErrorStory.args = {
   message: 'Some unexpected error',
+};
+
+type Planset = 'feasible';
+
+const plans: Record<Planset, RootState> = {
+  feasible: {
+    ...testRootState,
+    party: {
+      order: ['gandalf', 'legolas', 'gimli', 'boromir'],
+      characters: {
+        gandalf: {
+          id: 'gandalf',
+          name: 'Gandalf',
+          requiredSleepTime: 8 * 60,
+        },
+        legolas: {
+          id: 'legolas',
+          name: 'Legolas',
+          requiredSleepTime: 4 * 60,
+        },
+        gimli: {
+          id: 'gimli',
+          name: 'Gimli',
+          requiredSleepTime: 8 * 60,
+        },
+        boromir: {
+          id: 'boromir',
+          name: 'Boromir',
+          requiredSleepTime: 8 * 60,
+        },
+      },
+    },
+    plan: {
+      ...testRootState.plan,
+      fetchState: 'loaded',
+      totalTimeMinutes: 12 * 60,
+      watchOrder: ['first', 'second', 'third', 'fourth'],
+      watches: {
+        first: {
+          watchfulCharacters: ['legolas'],
+          sleepingCharacters: ['gandalf', 'gimli', 'boromir'],
+          minutesLength: 4 * 60,
+        },
+        second: {
+          watchfulCharacters: ['boromir', 'gandalf'],
+          sleepingCharacters: ['legolas', 'gimli'],
+          minutesLength: 2 * 60,
+        },
+        third: {
+          watchfulCharacters: ['gimli', 'gandalf'],
+          sleepingCharacters: ['legolas', 'boromir'],
+          minutesLength: 3 * 60,
+        },
+        fourth: {
+          watchfulCharacters: ['legolas', 'boromir'],
+          sleepingCharacters: ['gimli', 'gandalf'],
+          minutesLength: 3 * 60,
+        },
+      },
+    },
+  },
+};
+
+interface PlanPanelLoadedProps {
+  planset: Planset;
+}
+
+export const PlanPanelLoadedStory: Story<PlanPanelLoadedProps> = ({
+  planset,
+}) => {
+  return (
+    <TestAppWrapper state={plans[planset]}>
+      <PlanPanel />
+    </TestAppWrapper>
+  );
+};
+
+PlanPanelLoadedStory.args = {
+  planset: 'feasible',
+};
+
+PlanPanelLoadedStory.argTypes = {
+  planset: {
+    control: { type: 'radio', options: ['feasible'] },
+  },
 };
