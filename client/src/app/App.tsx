9ea724b4d6f5158da10863acd9d0ea32ab3fbd71
Carlos Castillo <castillo.st@gmail.com>
2023-05-01 16:17:30 +0200

    Add tanstack/react-query

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,15 +6,20 @@ import { Header } from './layout/Header'
 import { PartyTable } from '../features/party/PartyTable'
 import { NightWatchConfig } from '../features/config/NightWatchConfig'
 import { PlanPanel } from '../features/plan/PlanPanel'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const App: FC = () => (
     <Provider store={rootStore}>
-      <div className="min-h-screen bg-gradient-to-br from-white to-yellow-50">
-        <Header />
-        <PartyTable />
-        <NightWatchConfig />
-        <PlanPanel />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="min-h-screen bg-gradient-to-br from-white to-yellow-50">
+          <Header />
+          <PartyTable />
+          <NightWatchConfig />
+          <PlanPanel />
+        </div>
+      </QueryClientProvider>
     </Provider>
 )
 
