c27a056b9cef98d53176a0503bc7c508859567c1
Carlos Castillo <castillo.st@gmail.com>
2023-05-13 20:05:54 +0200

    ✨ Make PlanPanel component to use new plan generation hook
    - Use the new custom hook for generating plans
    - Remove unused imports and commented code

--- client/src/features/plan/PlanPanel.tsx
+++ client/src/features/plan/PlanPanel.tsx
@@ -1,29 +1,21 @@
 import { type FC } from 'react'
 import { TextButton } from '../../common/components/textButton/TextButton'
-// import { getMaxTotalTimeSpent } from '../config/getMaxTotalTimeSpent'
-// import { getCharacterList } from '../party/getCharacterList'
 import { PlanLoader } from './PlanLoader'
-import { getFetchStates } from './getFetchStates'
 import { PlanError } from './PlanError'
 import { PlanResult } from './PlanResult'
-import { useAppSelector } from '../../app/store/hooks'
+import { useGeneratePlan } from './useGeneratePlan'
+import { planServiceRest } from './planServiceRest'
 
 export const PlanPanel: FC = () => {
-  // TODO implement plan request using react query
-  // const maxTotalTimeSpent = useAppSelector(getMaxTotalTimeSpent)
-  // const characters = useAppSelector(getCharacterList)
-  // const generateNewPlan = () =>
-  //   dispatch(fetchPlanRequest(maxTotalTimeSpent, characters))
-  const generateNewPlan = () => { console.log('generateNewPlan not yet implemented') }
+  const { generatePlan, isErrored, isLoaded, isLoading } = useGeneratePlan(planServiceRest)
 
-  const { isLoading, isErrored, isLoaded } = useAppSelector(getFetchStates)
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-5 mx-auto">
         <TextButton
           className="text-white bg-gray-800 hover:bg-gray-900 focus:bg-gray-900 active:bg-gray-900"
           loading={isLoading}
-          onClick={generateNewPlan}
+          onClick={generatePlan}
         >
           Generate Plan
         </TextButton>
