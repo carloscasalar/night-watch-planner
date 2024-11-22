a9983a83416d3a4d75cec15be777c7e62ce7e249
Carlos Castillo <castillo.st@gmail.com>
2024-11-22 20:55:07 +0100

    Revert "Add GitHub Actions workflow for deploying client to GitHub Pages and update Vite config for base path"
    This reverts commit 077636fc9eb25bc2e98167617b164a3992190bcb.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/night-watch-planner/',
   plugins: [react()],
   resolve: {
     alias: {
